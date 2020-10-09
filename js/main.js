c673a2ffcda55d95f1eef5ac817fd27323f70f74
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-09 21:48:41 +0000

    keydown, keyup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
-  ('#select-menu').on('change',function(){
-    let name = $('#select-menu option:selcted').text(); 
-      let distance = $('#select-menu option:selcted').val();
-      let price = $('#select-menu option:selcted').data('price');
-     
-      if(distance){
-      $('#feedback-message').text('You are signing up for a ' + name + 'which costs' + price + ', to a distance of ' + distance + 'km');
-      } else {
-         $('#feedback-message').empty(); 
-      }
-  });
+   $('#input-name').on('keyup',function(){
+        let name = $(this).val();
+       $('#feedback-message').text('Pleased to meet you, ' + name);
+});
 });
