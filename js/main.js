0d21d718942f2c8dfd5393513521b4a7fbab2e9b
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-09 21:28:37 +0000

    change event

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  $('.box').on('click','.box-button',function(){
-     $(this).parent().toggleClass('highlight'); 
+  ('#select-menu').on('change',function(){
+    let name = $('#select-menu option:selcted').text(); 
+      let distance = $('#select-menu option:selcted').val();
+      let price = $('#select-menu option:selcted').data('price');
+     
+      if(distance){
+      $('#feedback-message').text('You are signing up for a ' + name + 'which costs' + price + ', to a distance of ' + distance + 'km');
+      } else {
+         $('#feedback-message').empty(); 
+      }
   });
 });
