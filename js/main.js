f37efa585263cd3d7746702869c5532931775a79
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-10 14:46:07 +0000

    creating items dynamically

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-   $('#input-name').on('keyup',function(){
-        let name = $(this).val();
-       $('#feedback-message').text('Pleased to meet you, ' + name);
-});
-    $('a').on('click',function(event){
-        event.preventDefault();
-         $('#feedback-message').text('That is fine');
-    });
+   $('#button-create-item').on('click',function(){
+      let name = $('#input-create-item').val();
+       $('#input-create-item').val('');
+       
+       let html ='';
+        html += '<div class="item">';
+          html += '<div class="name"> + name + </div>';
+           html += '<img src="assets/fui.jpg"></img>';
+           html += '<div class="description">loremnjnbdvb,hjb kjbvbevj jnevjnej ijveoi evijjevn</div>';
+           html += '<div class="price">499</div>';
+           html += '<button>Add to cart</button>';
+           html += '<br/>';
+           html += '<a href="#">More info</a>';
+           html += '<div class="more info">kjhck fjhlvn fhvphnw evihpihvn vipjnve lvipinv svpivjnpvj vjpjev 
+           '</div>';
+          html += '</div>';
+       
+       $('#container').append(html);
+   });
 });
